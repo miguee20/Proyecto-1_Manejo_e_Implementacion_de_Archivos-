e513bf70936b6dbf316f549e58c701ce97bd06e8
Iván Ordoñez <124651796+IvanOrdonez22@users.noreply.github.com>
2024-11-05 22:26:04 -0600

    commit importacion de librerias

---
+++ part_1.py
@@ -0,0 +1,7 @@
+import os
+import tkinter as tk
+from tkinter import filedialog, messagebox, scrolledtext
+from PIL import Image, ImageTk
+import time
+
+# Función para extraer información de un archivo GIF
